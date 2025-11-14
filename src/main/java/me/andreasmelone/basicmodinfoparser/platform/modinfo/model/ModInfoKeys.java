3848ef2815dea42b3dd38f124e36c44c3375e22c
apenasolinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-11-14 12:34:16 -0300

    feat: create `ModInfoKeys.java`

---
+++ src/main/java/me/andreasmelone/basicmodinfoparser/platform/modinfo/model/ModInfoKeys.java
@@ -0,0 +1,91 @@
+package me.andreasmelone.basicmodinfoparser.platform.modinfo.model;
+
+import java.util.Objects;
+
+/**
+ * Holds information about the available keys inside a mod's information file and their respective names.
+ *
+ * @see me.andreasmelone.basicmodinfoparser.platform.Platform
+ */
+public class ModInfoKeys {
+    public final String modIdKey;
+    public final String displayNameKey;
+    public final String versionKey;
+    public final String descriptionKey;
+    public final String logoFileKey;
+
+    public ModInfoKeys(
+            String modIdKey,
+            String displayNameKey,
+            String versionKey,
+            String descriptionKey,
+            String logoFileKey
+    ) {
+        this.modIdKey = modIdKey;
+        this.displayNameKey = displayNameKey;
+        this.versionKey = versionKey;
+        this.descriptionKey = descriptionKey;
+        this.logoFileKey = logoFileKey;
+    }
+
+    /**
+     * Helper method to create a forge-and-neoforge-compliant {@link ModInfoKeys}
+     *
+     * @return a forge-and-neoforge-compliant {@link ModInfoKeys}
+     */
+    public static ModInfoKeys forgeKeys() {
+        return new ModInfoKeys(
+                "modId",
+                "displayName",
+                "version",
+                "description",
+                "logoFile"
+        );
+    }
+
+    /**
+     * Helper method to create a fabric-and-quilt-compliant {@link ModInfoKeys}
+     *
+     * @return a fabric-and-quilt-compliant {@link ModInfoKeys}
+     */
+    public static ModInfoKeys fabricKeys() {
+        return new ModInfoKeys(
+                "id",
+                "name",
+                "version",
+                "description",
+                "icon"
+        );
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (this == other) return true;
+        if (this.getClass() != other.getClass()) return false;
+
+        ModInfoKeys castOther = (ModInfoKeys) other;
+
+        return castOther.modIdKey.equals(modIdKey)
+                && castOther.displayNameKey.equals(displayNameKey)
+                && castOther.versionKey.equals(versionKey)
+                && castOther.descriptionKey.equals(descriptionKey)
+                && castOther.logoFileKey.equals(logoFileKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modIdKey, displayNameKey, versionKey, descriptionKey, logoFileKey);
+    }
+
+    @Override
+    public String toString() {
+        return "ModInfoKeys{"
+                + "modIdKey=" + modIdKey
+                + ", displayNameKey=" + displayNameKey
+                + ", versionKey=" + versionKey
+                + ", descriptionKey=" + descriptionKey
+                + ", logoFileKey=" + logoFileKey
+                + "}";
+    }
+}
